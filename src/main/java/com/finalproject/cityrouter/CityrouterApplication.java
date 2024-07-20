7e499cf7b3d8954225fa8419480d88393e55de35
victoria_rusalovskaya <vika.rusalovskaya@gmail.com>
2024-07-20 15:46:40 +0400

    initial commit

---
+++ src/main/java/com/finalproject/cityrouter/CityrouterApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.cityrouter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityrouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityrouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalproject/cityrouter/CityrouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.cityrouter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityrouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
